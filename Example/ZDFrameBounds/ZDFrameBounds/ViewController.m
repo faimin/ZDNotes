076028283da81d808756c827f8621829a65bc537
符现超 <fuxianchao@gmail.com>
2017-03-30 15:17:11 +0800

    update FrameBounds

--- Example/ZDFrameBounds/ZDFrameBounds/ViewController.m
+++ Example/ZDFrameBounds/ZDFrameBounds/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (strong, nonatomic) UIView *zd_view;
+@property (strong, nonatomic) UIView *zd_view1;
+@property (strong, nonatomic) UIView *zd_view2;
 @property (nonatomic, assign) BOOL isResume;
 @end
 
@@ -19,30 +20,50 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    [self frameBounds];
+}
+
+UIKIT_STATIC_INLINE void zd_printViewCoordinateInfo(UIView *view) {
+    NSLog(@"\n frame = %@, bounds = %@, center = %@",
+          NSStringFromCGRect(view.frame),
+          NSStringFromCGRect(view.bounds),
+          NSStringFromCGPoint(view.center)
+    );
+}
+
+- (void)frameBounds {
     UIView *view1 = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 200, 200)];
     view1.backgroundColor = [UIColor redColor];
     [self.view addSubview:view1];
-    self.zd_view = view1;
+    self.zd_view1 = view1;
     
     UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
     view2.backgroundColor = [UIColor yellowColor];
     [view1 addSubview:view2];
+    self.zd_view2 = view2;
+    
+    zd_printViewCoordinateInfo(self.zd_view1);
+    zd_printViewCoordinateInfo(self.zd_view2);
 }
 
 - (IBAction)changeBounds:(UIButton *)sender {
     if (self.isResume) {
-        self.zd_view.bounds = (CGRect){0, 0, 200, 200};
+        self.zd_view1.bounds = (CGRect){0, 0, 200, 200};
     }
     else {
-        self.zd_view.bounds = (CGRect){20, 20, 200, 200};
+        self.zd_view1.bounds = (CGRect){20, 20, 150, 150};
     }
     self.isResume = !self.isResume;
+    
+    zd_printViewCoordinateInfo(self.zd_view1);
+    zd_printViewCoordinateInfo(self.zd_view2);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 
 @end
